27836f1c7223fae8eb824f5667bd8a5a7c9f956d
Raghul Krishnan <raghul.sathya@gmail.com>
2020-05-13 11:57:28 -0400

    Updates

---
+++ precache-manifest.32d48e86a6668274ed1a1f94def85d0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dabe217f1297f877e394712e50ea237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a83ee4698f8bd7878ae",
+    "url": "/static/css/2.f7da8290.chunk.css"
+  },
+  {
+    "revision": "7f98e093031e1bbf1470",
+    "url": "/static/css/main.1e15b076.chunk.css"
+  },
+  {
+    "revision": "7a83ee4698f8bd7878ae",
+    "url": "/static/js/2.d5156b60.chunk.js"
+  },
+  {
+    "revision": "ea163f3125882d32ccf04be4ce93633f",
+    "url": "/static/js/2.d5156b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f98e093031e1bbf1470",
+    "url": "/static/js/main.87c70b67.chunk.js"
+  },
+  {
+    "revision": "170012d5f8a2d3d3ce6c",
+    "url": "/static/js/runtime-main.fda75f35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yoda/precache-manifest.614dde91b118ba6e781f5f724d78280d.js"
+  "/precache-manifest.32d48e86a6668274ed1a1f94def85d0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yoda/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
